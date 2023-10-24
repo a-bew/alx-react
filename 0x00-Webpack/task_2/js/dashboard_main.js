06126250cb8484ff705b65fd519faa96292a120d
Adetola Bewaji <adetola013@gmail.com>
2023-10-24 11:02:13 +0100

    js/dashboard_main.js updated

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,11 +1,12 @@
-const $ = require('jquery')
+import '../css/main.css';
+const $ = require('jquery');
 const _ = require('lodash');
 
-$('body').append('<p>Holberton Dashboard</p>')
-$('body').append('<p>Dashboard data for the students</p>')
-$('body').append('<button>Click here to get started</button>')
-$('body').append("<p id='count'></p>")
-$('body').append('<p>Copyright - Holberton School</p>')
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append("<p id='count'></p>");
+$('body').append('<p>Copyright - Holberton School</p>');
 
 const updateCounter = () => {
     let times = $('#count').html() || 0;
@@ -13,7 +14,7 @@ const updateCounter = () => {
         times++;
         $('#count').html(`${times} clicks on the button`);
     });
-}
+};
 
 _.debounce(updateCounter, 500);
 updateCounter();
