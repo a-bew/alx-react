01b8997f1c8f613ffab0577c1bce480b1b810a4d
Adetola Bewaji <adetola013@gmail.com>
2023-10-24 11:05:49 +0100

    webpack updated

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     },
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public'),
+        path: path.resolve(__dirname, 'public')
     },
     performance: {
         maxAssetSize: 1000000,
@@ -22,13 +22,12 @@ module.exports = {
                 test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
                 use: [
                     {
-                        loader: 'file-loader', // Specify the loader as a string, not in an array
+                        loader: ['file-loader', 'image-webpack-loader'],
                         options: {
                             bypassOnDebug: true,
                             disable: true,
                         },
                     },
-                    'image-webpack-loader',
                 ],
             },
         ],
